e06be15585bd907141cbff19b1f4f52c9dcbe602
MatthewKenway <matthew.kenway.mk@gmail.com>
2024-02-08 20:19:02 +1200

    A refactor that removed an extra intermediate class. Rename classes. Adding a description to classes. Small refactor of argument names of methods and variables.
    Objects of the GithubClient class now generate a list of ready LibraryPackage objects into the attribute when parsing the required libraries for the repository during object initialization, instead of creating a list of strings describing the characteristics of the required libraries into the attribute

--- main.py
+++ main.py
@@ -4,55 +4,65 @@ import requests
 
 
 class GitClient(ABC):
-    URL_PATTERN: str
+    """
+    Abstract class template for classes to work with different git repositories
+    Describes methods for parsing the list of required libraries for a project in the repository
+    """
 
-    def __init__(self, repo_url):
+    URL_PATTERN: str
+    API_URL_TEMPLATE: str
+
+    def __init__(self, repo_url: str) -> None:
         self.repo_url = repo_url
         self.requirements_list = self.get_requirements_list()
 
     @abstractmethod
-    def _get_file_content(self, files_urls):
+    def _get_files_content(self, files_urls: list) -> list[str]:
         pass
 
     @abstractmethod
-    def _get_repo_api_url(self):
+    def _get_repo_api_url(self) -> str:
         pass
 
     @abstractmethod
-    def _search_requirements_files(self, repo_api_url):
+    def _search_requirements_files(self, repo_api_request: requests.models.Response) -> list[str]:
         pass
 
     @abstractmethod
-    def get_requirements_list(self):
+    def get_requirements_list(self) -> list:
         pass
 
 
 class GithubClient(GitClient):
     URL_PATTERN = r'(?:https?://)?github.com/([^/]+)/([^/]+)'
+    API_URL_TEMPLATE = 'https://api.github.com/repos/'
 
     def _get_repo_api_url(self):
         matches = re.search(self.URL_PATTERN, self.repo_url)
 
         if matches:
             user_repo = f'{matches.group(1)}/{matches.group(2)}'
-            return requests.get(url='https://api.github.com/repos/' + user_repo + '/contents/')
+            repo_api_url = self.API_URL_TEMPLATE + user_repo + '/contents/'
+            return repo_api_url
 
-    def _search_requirements_files(self, repo_api_url):
-        for repo_content in repo_api_url.json():
+    def _search_requirements_files(self, repo_api_request):
+        files = []
+        for repo_content in repo_api_request.json():
             if 'requirements' in repo_content['name'] and repo_content['type'] == 'file':
-                return [repo_content['download_url']]
+                files.append(repo_content['download_url'])
+
             elif 'requirements' in repo_content['name'] and repo_content['type'] == 'dir':
                 dir_api_url = requests.get(url=repo_content['url'])
-                files = []
                 for dir_content in dir_api_url.json():
                     if '.txt' in dir_content['name']:
                         files.append(dir_content['download_url'])
-                return files
+        return files
 
-    def _get_file_content(self, files_urls):
+    def _get_files_content(self, files_urls):
         content = []
         for file_url in files_urls:
-            result = requests.get(file_url).text.split(sep='\n')
+            file_data = requests.get(file_url).text
+            result = file_data.split(sep='\n')
             while '' in result:
                 result.remove('')
             content.extend(result)
@@ -60,12 +70,22 @@ class GithubClient(GitClient):
 
     def get_requirements_list(self):
         repo_api_url = self._get_repo_api_url()
-        requirement_files = self._search_requirements_files(repo_api_url=repo_api_url)
-        requirement_files_content = self._get_file_content(files_urls=requirement_files)
-        return requirement_files_content
+        repo_api_request = requests.get(url=repo_api_url)
+
+        requirement_files = self._search_requirements_files(repo_api_request=repo_api_request)
+        requirement_files_content = self._get_files_content(files_urls=requirement_files)
+
+        source = PackagesSource()
+        requirement_libraries = source.get_libraries(libs_str_list=requirement_files_content)
+
+        return requirement_libraries
 
 
-class Package:
+class LibraryPackage:
+    """
+    A class that packages the library name and its version requirements into a single object
+    The class also overrides the method for comparing its objects, considering the comparison mark and the version
+    """
 
     def __init__(self, name, delimeter, version):
         self.name = name
@@ -111,84 +131,79 @@ class Package:
             if other.delimeter == '>' and (self.delimeter == '<=' or self.delimeter == '<'):
                 return self.version > other.version
 
-
-class PackageInsecureLimits(Package):
-
-    def __init__(self, name, delimeter_first_requirement, version_first_requirement,
-                 delimeter_second_requirement='>', version_second_requirement='0'):
-        self.name = name
-        self.first_requirement = Package(name=name, delimeter=delimeter_first_requirement,
-                                         version=version_first_requirement)
-        self.second_requirement = Package(name=name, delimeter=delimeter_second_requirement,
-                                          version=version_second_requirement)
-
-    def __eq__(self, other):
-        if other == self.first_requirement:
-            if other == self.second_requirement:
-                return True
-
-        return False
-
-    def __str__(self):
-        return (f'{self.first_requirement} and '
-                f'{self.second_requirement}')
+    def __ne__(self, other):
+        return not self.__eq__(other=other)
 
 
-class VulnerabilityPackage(Package):
+class Vulnerability:
+    """
+    A class for storing information about vulnerabilities in InsecureLibrary-class objects.
+    Objects contain: the name of the library to which the vulnerability belongs, the description of the vulnerability
+    and the scope of versions in which the vulnerability is present
+    """
+
     INSECURE_SPECS_PATTERN = r'([<>=!]+)\s*(\d+(\.\d+)*)'
 
     def __init__(self, name, advisory, specs):
         self.name = name
         self.advisory = advisory
         self.version_limits = []
-        self._version_limits(specs=specs)
+        self._get_version_limits(specs=specs)
 
     def __eq__(self, other):
         for limit in self.version_limits:
-            if limit == other:
-                return True
+            if limit != other:
+                return
+        return True
 
-    def _version_limits(self, specs):
+    def __str__(self):
+        return self.advisory
+
+    def _get_version_limits(self, specs):
         for spec in specs:
             spec = spec.split(sep=',')
-            args = [self.name]
+            kwargs = dict([('name', self.name)])
             for version in spec:
                 matches = re.match(self.INSECURE_SPECS_PATTERN, version)
-                args.append(matches.group(1))
-                args.append(matches.group(2))
-            self.version_limits.append(PackageInsecureLimits(*args))
+                kwargs['delimeter'] = matches.group(1)
+                kwargs['version'] = matches.group(2)
+                self.version_limits.append(LibraryPackage(**kwargs))
 
 
-class InsecureLibraryPackage(Package):
+class InsecureLibrary:
+    """
+    A class that packages the library name and its vulnerabilities list into a single object
+    Has a method to search for vulnerabilities included in the LibraryPackage-class object specified in the argument
+    """
 
     def __init__(self, name):
         self.name = name
         self.vulnerabilities = []
 
     def _add_vulnerability(self, advisory, specs):
-        self.vulnerabilities.append(VulnerabilityPackage(name=self.name, advisory=advisory, specs=specs))
+        self.vulnerabilities.append(Vulnerability(name=self.name, advisory=advisory, specs=specs))
 
-    def match_vulnerability(self, package):
-        if package.name == self.name:
+    def match_vulnerability(self, lib_package):
+        if lib_package.name == self.name:
             result = []
             for vulnerability in self.vulnerabilities:
-                if vulnerability == package:
+                if vulnerability == lib_package:
                     result.append(vulnerability)
             if result:
                 return result
 
 
 class PackagesSource:
+    """
+    Source class for creating LibraryPackage-class objects from strings with a library description
+    """
+
     LIB_STRING_PATTERN = r'([\w\s\W]+?)\s*([<>=!]+)\s*(\d+(\.\d+)*)'
 
-    def __init__(self, client):
-        self.client = client
-
-    def get_libraries(self):
-        file_contents = self.client.requirements_list
+    def get_libraries(self, libs_str_list):
         libraries = []
 
-        for lib_string in file_contents:
+        for lib_string in libs_str_list:
             matches = re.match(self.LIB_STRING_PATTERN, lib_string)
             if matches:
                 name = matches.group(1)
@@ -198,31 +213,31 @@ class PackagesSource:
                 name = lib_string
                 delimeter = '>'
                 version = '0'
-            libraries.append(Package(name=name, delimeter=delimeter, version=version))
+            name = name.lower()
+            libraries.append(LibraryPackage(name=name, delimeter=delimeter, version=version))
 
         return libraries
 
 
-class PackagesInsecureSource:
+class InsecureLibrariesSource:
+    """
+    Source class for creating objects that store a list of library vulnerabilities. Takes a list of
+    LibraryPackage-class objects as input and outputs a list of InsecureLibrary-class objects with vulnerabilities
+    for libraries from the input
+    """
+
     LIB_STRING_PATTERN = r'^([\w\s\W]+?)(?:\s*([<>=!]+)\s*(\d+(\.\d+)*))?$'
-    INSECURE_FULL = requests.get(
+    INSECURE_FULL_CATALOGUE = requests.get(
         url='https://raw.githubusercontent.com/pyupio/safety-db/master/data/insecure_full.json').json()
 
-    def __init__(self, client):
-        self.client = client
-
-    def get_libraries(self):
-        file_contents = self.client.requirements_list
+    def get_libraries(self, requirement_libs_list):
         result = []
 
-        for lib_string in file_contents:
-            matches = re.match(self.LIB_STRING_PATTERN, lib_string)
-            name = matches.group(1)
-            if name in self.INSECURE_FULL or name.lower() in self.INSECURE_FULL:
-                if name.lower() in self.INSECURE_FULL:
-                    name = name.lower()
-                insecure_package = InsecureLibraryPackage(name=name)
-                for vulnerability in self.INSECURE_FULL[name]:
+        for library in requirement_libs_list:
+            name = library.name
+            if name in self.INSECURE_FULL_CATALOGUE:
+                insecure_package = InsecureLibrary(name=name)
+                for vulnerability in self.INSECURE_FULL_CATALOGUE[name]:
                     insecure_package._add_vulnerability(advisory=vulnerability['advisory'],
                                                         specs=vulnerability['specs'])
                 result.append(insecure_package)
@@ -231,14 +246,18 @@ class PackagesInsecureSource:
 
 
 client = GithubClient(repo_url='https://github.com/PyGithub/PyGithub/tree/main')
-source = PackagesSource(client=client)
-requirement_libraries = source.get_libraries()
-insecurity_source = PackagesInsecureSource(client=client)
-insecurity_libraries = insecurity_source.get_libraries()
+
+insecurity_source = InsecureLibrariesSource()
+insecurity_libraries = insecurity_source.get_libraries(requirement_libs_list=client.requirements_list)
+
+vulnerabilities = []
 for insecure_library in insecurity_libraries:
-    for requirement_library in requirement_libraries:
-        vulnerability_packages = insecure_library.match_vulnerability(package=requirement_library)
-        if vulnerability_packages:
-            for package in vulnerability_packages:
-                print(package.advisory)
+    for requirement_library in client.requirements_list:
+        if insecure_library.name == requirement_library.name:
+            vulnerabilities.extend(insecure_library.match_vulnerability(lib_package=requirement_library))
+
+if vulnerabilities:
+    for package in vulnerabilities:
+        print(package.advisory)
+        print('\n')
                 
